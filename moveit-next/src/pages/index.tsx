f8e6a43581278e3e3e021ad92562615ddff7d456
João Vitor de Faria <jvfaria015@gmail.com>
2021-02-25 00:35:38 -0300

    context inserted

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -2,7 +2,9 @@ import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
+import { ChallengeBox } from '../components/ChallengeBox';
 import styles from '../styles/pages/Home.module.css';
+
 
 import Head from 'next/head';
 
@@ -18,8 +20,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div>
 
+        <div>
+          <ChallengeBox />
         </div>
       </section>
     </div>
