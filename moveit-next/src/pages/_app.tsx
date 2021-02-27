1baf1279c1cf24623bad2e074c36c6e67beb1c21
João Vitor de Faria <jvfaria015@gmail.com>
2021-02-27 12:52:34 -0300

    app

--- moveit-next/src/pages/_app.tsx
+++ moveit-next/src/pages/_app.tsx
@@ -1,13 +1,12 @@
 import '../styles/global.css';
 
-import { ChallengesContext, ChallengesProvider } from '../contexts/ChallengesContext';
-import { useContext } from 'react';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
 
 function MyApp({ Component, pageProps }) {
- 
+
   return (
     <ChallengesProvider>
-      <Component {...pageProps} />
+        <Component {...pageProps} />
     </ChallengesProvider>
   )
 }
