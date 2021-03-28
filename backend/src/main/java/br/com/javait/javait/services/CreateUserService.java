d18555aea142add3af5666c94dd2d802d25f8639
João Vitor de Faria <jvfaria015@gmail.com>
2021-03-28 19:21:36 -0300

    services

---
+++ backend/src/main/java/br/com/javait/javait/services/CreateUserService.java
@@ -0,0 +1,26 @@
+package br.com.javait.javait.services;
+
+import br.com.javait.javait.model.User;
+import br.com.javait.javait.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateUserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public ResponseEntity<User> create(User user) { 
+        User u = new User();
+        u.setName(user.getName());
+        u.setEmail(user.getEmail());
+
+        String encoded = new BCryptPasswordEncoder().encode(user.getPassword());
+        u.setPassword(encoded);
+        userRepository.save(u);
+
+        return ResponseEntity.ok().body(user);
+    }
+}
