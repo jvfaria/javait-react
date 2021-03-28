cb8d8f4aa5bcac1d8bb25c99ad1541d79bcb6521
João Vitor de Faria <jvfaria015@gmail.com>
2021-03-28 19:22:23 -0300

    update 0.0.1 28/03

--- backend/src/main/resources/db/migration/V001__create_table_users.sql
+++ backend/src/main/resources/db/migration/V001__create_table_users.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users(
-    id serial primary key,
+    id varchar(256) primary key,
     name varchar(256) not null,
     email varchar(256) not null,
     password varchar(256) not null
